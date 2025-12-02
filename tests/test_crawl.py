a2ce37d8f796e3b438e08c91d2b4080f6a63901d
Maine <sdjohnson0215@gmail.com>
2025-12-02 10:05:29 -0600

    Add function and file comment headers

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -1,8 +1,25 @@
+# ###########################################
+# Name: Shayene Johnson
+# Assignment: 8
+# Purpose: Tester for crawl.py
+#          run function
+# ###########################################
+
 import typing as t
 
 from minicrawler.crawl import run
 
 def test_run_zero_max_pages_produces_no_results() -> None:
+    """
+   This function tests that the crawl.run function
+   produces no results when max_pages is less than
+   the minimum allowed value.
+
+   :param na: na
+   :return None: na
+   :exception na: na
+   :note na
+   """
     rows: t.List[dict] = list(
         run(
             seed="https://example.com",
