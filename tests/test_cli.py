4b798c44a3f1eaaebc63bf35b7c8ea682873e1a3
Maine <sdjohnson0215@gmail.com>
2025-12-14 00:23:06 -0600

    "Add test_cli.py file to test cli.py."

---
+++ tests/test_cli.py
@@ -0,0 +1,8 @@
+# ###########################################
+# Name: Shayene Johnson
+# Assignment: Final Project
+# Purpose: Tester for cli.py
+#          parser + main dispatch
+#          for crawl/scrape/summary/instagram
+# ###########################################
+
